2af42035593011c6619fca9e523f75c9556ed3bc
maxmiau <miaumax@gmail.com>
2019-05-23 16:33:46 -0500

    Primera cosa que sale bien en el front-end

--- resources/views/paginas/index.blade.php
+++ resources/views/paginas/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1> Esto es layouts bouy</h1>
+    <div class="jumbotron text-center">
+        <h1>Bienvenidos al sistema de asistencias</h1>
+        <p>Este es un sistema basico de conteo de asistencias elaborado en laravel</p>
+        <p><a class="btn btn-primary btn-lg" href="/login" role="button">login</a>
+            <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
+        </p>
+    </div>
 @endsection
