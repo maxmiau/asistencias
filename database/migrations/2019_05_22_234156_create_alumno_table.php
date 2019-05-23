1127166ea1077d094c50361e20efbf70f2135ed5
maxmiau <miaumax@gmail.com>
2019-05-22 21:10:47 -0500

    Primer Commit

---
+++ database/migrations/2019_05_22_234156_create_alumno_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlumnoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alumno', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('grado')->nullable();
+            $table->string('carrera')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alumno');
+    }
+}
